34ada2064c545ffdcd89a4590a84edf8b0c73efd
Raka <kaiwest@bigpond.com>
2025-06-18 05:40:16 +0000

    [automatic commit] 2025-06-18 05:40:16 UTC 121.45.82.153

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
     socketPath: 'var/run/mysqld/mysqld.sock',
-    host: '127.0',
+    host: '127.0.0.1',
     user: 'root',
     password: 'mypassword',
     database: 'textbook_marketplace'
