859506da4173c9b23ead5a3dbcd428b78c1b1e0b
Raka <kaiwest@bigpond.com>
2025-06-18 09:21:45 +0000

    [automatic commit] 2025-06-18 09:21:45 UTC 49.178.106.87

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -16,7 +16,7 @@ router.get('/items', async (req, res) => {
 
 router.get('/messages', async (req, res) => {
     const [rows] = await db.query(`
-        SELECT m.message_text, m.sent_at, b.title, u.name AS buyer_name
+        SELECT m.message_text, m.sent_at, b.title, u.username AS buyer_name
         FROM Messages m
         JOIN Listings l ON m.listing_id = l.listing_id
         JOIN Books b ON l.book_id = b.book_id
