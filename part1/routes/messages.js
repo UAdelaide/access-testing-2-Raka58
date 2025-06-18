42cc2a28b71b919c9310f19434e2a8e870d1e5e6
Raka <kaiwest@bigpond.com>
2025-06-18 05:50:59 +0000

    [automatic commit] 2025-06-18 05:50:59 UTC 121.45.82.153

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -20,7 +20,7 @@ router.post('/messages', async (req, res) => {
     await db.query(`
         INSERT INTO Messages (sender_id, receiver_id, listing_id, message_text, sent_at)
         VALUES (?, ?, ?, ?, NOW())
-        `);
-})
+        `, []);
+});
 
 module.exports = router;
