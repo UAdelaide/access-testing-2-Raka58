b9dce98e0342f99385449ac2c2ecf28195c11fb5
Raka <kaiwest@bigpond.com>
2025-06-18 08:56:25 +0000

    [automatic commit] 2025-06-18 08:56:25 UTC 49.178.106.87

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -7,7 +7,7 @@ var CURRENT_SELLER_ID = 3;
 
 router.get('/items', async (req, res) => {
     const [rows] = await db.query(`
-        SELECT l.listing_id, b.title, u.username AS seller_name
+        SELECT l.listing_id, b.title, u.username AS seller_name, l.seller_id
         FROM Listings l
         JOIN Books b ON l.book_id = b.book_id
         JOIN Users u ON l.seller_id = u.user_id
