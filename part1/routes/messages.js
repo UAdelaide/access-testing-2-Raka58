ae750b781b635a395e7e9abefa5bf2ea8865d29a
Raka <kaiwest@bigpond.com>
2025-06-18 05:32:39 +0000

    [automatic commit] 2025-06-18 05:32:39 UTC 121.45.82.153

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
-var CURRENT_BUYER_ID = 1;
-var CURRENT_SELLER_ID = 2;
+var CURRENT_BUYER_ID = 2;
+var CURRENT_SELLER_ID = 3;
 
+router.get('/items')
