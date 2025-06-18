d68756ac99c113e8bcbb1b3a6b5c59fccceaac57
Raka <kaiwest@bigpond.com>
2025-06-18 09:15:15 +0000

    [automatic commit] 2025-06-18 09:15:15 UTC 49.178.106.87

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -16,8 +16,8 @@ router.get('/items', async (req, res) => {
 
 router.get('/messages', async (req, res) => {
     const [rows] = await db.query(`
-
-        `)
+        SELECT m.message_text, m.sent_at, b.
+        `);
 });
 
 router.post('/messages', async (req, res) => {
