0617f1a5b4a9f23f189c744de0eb946dde6b31b2
Raka <kaiwest@bigpond.com>
2025-06-18 05:37:10 +0000

    [automatic commit] 2025-06-18 05:37:10 UTC 121.45.82.153

--- part1/app.js
+++ part1/app.js
@@ -17,6 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use
+app.use('/api', messageRouter); //==========
 
 module.exports = app;
